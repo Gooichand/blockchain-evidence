2ce1c49fe9080b910b163fe3c5615267155422af
Abhin Vaidya <abhinavvaidya2005@gmail.com>
2026-01-06 02:10:46 +0530

    refactor: update controllers/index.js to include authController
    - Added authController to the central export file
    - Ensures consistency with the maintainer's suggested structure

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ module.exports = {
     userController: require('./user.controller'),
     caseController: require('./case.controller'),
     evidenceController: require('./evidence.controller'),
-    dashboardController: require('./dashboard.controller')
+    dashboardController: require('./dashboard.controller'),
+    authController: require('./auth.controller')
 };
